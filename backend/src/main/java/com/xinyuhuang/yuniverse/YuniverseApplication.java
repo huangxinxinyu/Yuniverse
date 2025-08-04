cf77a44e98504018f84a98e021c079e2083b6929
huangxinxinyu <xinyuhimself@gmail.com>
2025-08-04 21:39:21 +1000

    first commit

---
+++ backend/src/main/java/com/xinyuhuang/yuniverse/YuniverseApplication.java
@@ -0,0 +1,13 @@
+package com.xinyuhuang.yuniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuniverseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/xinyuhuang/yuniverse/YuniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinyuhuang.yuniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
